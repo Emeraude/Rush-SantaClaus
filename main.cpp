7a01dcc916e83cb872561b561c5d9cd38ebdd069
duques_g <duques_g@localhost.localdomain>
2015-01-17 19:36:14 +0100

    main

--- main.cpp
+++ main.cpp
@@ -1,4 +1,21 @@
 
 #include "LittlePony.hpp"
-#include "Toy.hpp"
- 
+#include "Teddy.hpp"
+#include "Box.hpp"
+#include "GiftPaper.hpp"
+
+
+int		main(void) {
+  LittlePony	Pony("pony");
+  Teddy		Teddy("teddy");
+
+  Box		Box("box");
+  GiftPaper	GiftPaper("giftpaper");
+
+  Wrap		Wrap("WRAP");
+  Toy		Toy("toy");
+
+
+  return 0;
+}
+
