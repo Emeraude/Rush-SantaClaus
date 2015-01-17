c2697da22ea5d15f0e86241537756f2725e954b4
Arthur Poulet <arthur.poulet@cryptolab.net>
2015-01-17 23:18:51 +0100

    add IConveyorBelt

---
+++ IConveyorBelt.hpp
@@ -0,0 +1,16 @@
+#ifndef ICONVERYORBELT_HPP
+# define ICONVERYORBELT_HPP
+
+# include "Object.hpp"
+
+class IConveyorBelt
+{
+public:
+  virtual Object *Take() = 0;
+  virtual bool Put(Object*) = 0;
+  virtual bool IN() = 0;
+  virtual void OUT() = 0;
+  virtual std::string* const & Look() = 0;
+};
+
+#endif
