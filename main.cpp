9d31faa14bd69813d0173ce3c9fe1215460ad7ba
kalatz_a <kalatz_a@epitech.eu>
2015-01-17 19:02:41 +0100

    add tests

--- main.cpp
+++ main.cpp
@@ -1,8 +1,20 @@
 
-#include "LittlePony.hpp"
+#include "Object.hpp"
 #include "Toy.hpp"
- 
+#include "Teddy.hpp"
+#include "LittlePony.hpp"
+#include <stdio.h>
+
 int	main(void)
 {
+
+  printf("\n_____________\n# Teddy class\n\n");
+  Teddy PedoBear;
+  PedoBear.isTaken();
+
+  printf("\n____________\n# LittlePony class\n\n");
+  LittlePony Brownie;
+  Brownie.isTaken();
+
   return 0;
 }
