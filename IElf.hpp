3997c29fe83418e80965ef14bc78d0de61065b43
Arthur Poulet <arthur.poulet@cryptolab.net>
2015-01-18 04:13:22 +0100

    fix IElf : add all function to protected field

--- IElf.hpp
+++ IElf.hpp
@@ -6,10 +6,21 @@
 
 class IElf
 {
-public:
+protected:
   virtual ~IElf() {}
   virtual void startWork() = 0;
   virtual bool sendGift() = 0;
+  virtual bool CheckIfBoxAvailable() const = 0;
+  virtual bool CheckIfPaperAvailable() const = 0;
+  virtual bool CheckIfToyAvailable() const = 0;
+  virtual bool CheckIfGiftAvailable() const = 0;
+  virtual bool TableHasPlace() const = 0;
+  virtual void EnumTable() const = 0;
+  virtual Object *TakeFirstToy() = 0;
+  virtual Object *TakeFirstBox() = 0;
+  virtual Object *TakeFirstPaper() = 0;
+  virtual void MakeAGift() = 0;
+  virtual bool TakeAWarp() = 0;
 };
 
 #endif
