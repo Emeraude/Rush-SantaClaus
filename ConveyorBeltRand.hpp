f7346fd436414b36cc62ea5f1cd85a6f323d0918
broggi_t <broggi_t@epitech.eu>
2015-01-18 05:20:00 +0100

    adding random classes + fix name in conveyorbelt of santa claus

---
+++ ConveyorBeltRand.hpp
@@ -0,0 +1,13 @@
+#ifndef CONVEYORBELTRAND_HPP
+# define CONVEYORBELTRAND_HPP
+
+# include "ConveyorBeltPePeNoel.hpp"
+
+class ConveyorBeltRand : public ConveyorBeltPePeNoel {
+public:
+  ConveyorBeltRand();
+  ~ConveyorBeltRand();
+  bool IN();
+};
+
+#endif
